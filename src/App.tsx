29e1d23906288f0e800fbf2598b2ed06ac40c92a
nechamdod <nechamdod@gmail.com>
2023-07-12 14:15:01 +0300

    init: init material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Button} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <div className='bg-amber-800'>AAAAA</div>
+        <Button className='bg-amber-800'>Butt</Button>
     </div>
   );
 }
